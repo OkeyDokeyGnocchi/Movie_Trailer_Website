c39b7c85420ad2c108b65e1a3b23569e66153910
Brandon <brandon@Brandons-MacBook-Pro.local>
2017-10-20 13:05:23 -0700

    Initialize the repo and add the existing files.

---
+++ entertainment_center.py
@@ -0,0 +1,35 @@
+import media
+import fresh_tomatoes
+
+rocky_horror = media.Movie("Rocky Horror", # Sorry that I made the title short, the full title took 2 slots.
+                            "The story of a young couple's encounter with a bizarre scientist.",
+                            "http://t1.gstatic.com/images?q=tbn:ANd9GcSw4XUIY54oe7kXffs5aHPny8lz4v-j1A4XuLRC4ebfpHOHUfLL",
+                            "https://www.youtube.com/watch?v=Pgx1QZFNMz8",
+                            "R", "80%")
+donnie_darko = media.Movie("Donnie Darko", "A troubled teen follows a rabbit into madness.",
+                            "https://upload.wikimedia.org/wikipedia/en/d/db/Donnie_Darko_poster.jpg",
+                            "https://www.youtube.com/watch?v=rPeGaos7DB4",
+                            "R", "86%")
+zoolander = media.Movie("Zoolander",
+                        "Male model Derek Zoolander finds himself in the middle of a major conspiracy.",
+                        "https://upload.wikimedia.org/wikipedia/en/7/7c/Movie_poster_zoolander.jpg",
+                        "https://www.youtube.com/watch?v=YtQq0T3ExLs",
+                        "PG-13", "64%")
+the_producers = media.Movie("The Producers",
+                            "A washed up theatre producer and timid accountant scheme to get rich.",
+                            "https://upload.wikimedia.org/wikipedia/en/4/49/The_Producers_%282005%29.jpg",
+                            "https://www.youtube.com/watch?v=u36iNj52rac",
+                            "PG-13", "50%")
+road_to_el_dorado = media.Movie("The Road to El Dorado",
+                                "Two con artists discover the golden city of El Dorado.",
+                                "https://upload.wikimedia.org/wikipedia/en/f/fa/Road_to_el_dorado_ver3.jpg",
+                                "https://www.youtube.com/watch?v=JcOfJwN0bdY",
+                                "PG", "69%")
+the_dark_knight = media.Movie("The Dark Knight",
+                                "Batman sets out to dismantle the remaining criminal organizations that plague the city streets.",
+                                "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+                                "https://www.youtube.com/watch?v=kmJLuwP3MbY",
+                                "PG-13", "94%")
+
+movies = [rocky_horror, donnie_darko, zoolander, the_producers, road_to_el_dorado, the_dark_knight]
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+class Movie():
+    """This class is used to generate the information for movies that
+    the program needs to create the movie website fresh_tomatoes."""
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                trailer_youtube, critic_score, movie_rating):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.score = critic_score
+        self.rating = movie_rating
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
