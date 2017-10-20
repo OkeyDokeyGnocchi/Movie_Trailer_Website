a09022082b4ae4f71afd84574eca3109520d6f38
Brandon <brandon@Brandons-MacBook-Pro.local>
2017-10-20 14:01:09 -0700

    Update media.py to meet PEP8 req's.

--- media.py
+++ media.py
@@ -1,11 +1,12 @@
 import webbrowser
 
+
 class Movie():
     """This class is used to generate the information for movies that
     the program needs to create the movie website fresh_tomatoes."""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
-                trailer_youtube, critic_score, movie_rating):
+                 trailer_youtube, critic_score, movie_rating):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
